ff36e04044d82065b4208b63a7d8646b6ba99d12
Bennett Greenberg <bennett.greenberg@rutgers.edu>
2025-06-03 08:52:50 -0500

    add EtaPrime4Mu PlutoReader

---
+++ Configuration/Generator/python/PlutoReader_EtaPrimeTo4Mu_pythia8_cfi.py
@@ -0,0 +1,33 @@
+import FWCore.ParameterSet.Config as cms
+
+generator = cms.EDFilter("Pythia8PlutoReaderGun",
+#generator = cms.EDFilter("Pythia8PlutoReaderExpGun",
+
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+
+    PGunParameters = cms.PSet(
+        Filename = cms.string('GeneratorInterface/Pythia8Interface/test/pluto_EtaPrimeTo4Mu_1M_events.csv'),
+        ParticleID = cms.vint32(13),
+        MakeDisplaced = cms.bool(False),
+        MinPhi = cms.double(-3.14159265359),
+        MaxPhi = cms.double(3.14159265359),
+        MinPt = cms.double(10.0),
+        MaxPt = cms.double(65.0),
+        MinEta = cms.double(-2.4),
+        MaxEta = cms.double(2.4),
+        MinProdRadius = cms.double(0.0),
+        MaxProdRadius = cms.double(10.0),
+        NumDaughters = cms.int32(4)
+        ),
+
+    PythiaParameters = cms.PSet(
+        py8ZDecaySettings = cms.vstring(  '23:onMode = off', # turn OFF all Z decays
+            '23:onIfAny = 15'  # turn ON Z->tautau
+            ),
+        parameterSets = cms.vstring(  
+            #'py8ZDecaySettings' 
+            )
+        )
+    )
